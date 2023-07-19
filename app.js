a331d529a5699aa79c609dff718a1336cc04070a
Meet Thummar <thummarmeet15@gmail.com>
2023-07-19 19:24:17 +0530

    Added authentication using passport module. also session and cookies are added.

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express()
 
@@ -14,6 +16,15 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.use(session({
+    secret: "Indian Institute of Technology Bombay.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
@@ -21,10 +32,15 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-// // Apply secret always before making model.
-// userSchema.plugin(encrypt, { secret : process.env.SECRET, encryptedFields: ["password"] });
+
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
     res.render("home");
@@ -38,53 +54,56 @@ app.get("/register", function(req, res) {
     res.render("register");
 });
 
+app.get("/secrets", function(req, res) {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.get("/logout", function(req,res) {
+    req.logout();
+    res.redirect("/");
+})
+
 // When user registers the post request is sent to "/register" route
-let saltRounds = 10;
 app.post("/register",  function(req, res) {
 
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-        try {
-            newUser.save();
-            res.render("secrets");
-        } catch (err) {
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+        if (err) {
             console.log(err);
-        };
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
+        }
     });
+    
 
 });
 
 // Post request from login form
 app.post("/login", async function(req, res) {
-    
 
-    try {
-        let mail = req.body.username;
-        let pwd = req.body.password;
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
 
-        // mongoose-encrypt will automatically decrypts the password here
-        const person = await User.findOne({ 'email' : mail});
-        if (person !== null) {
-            bcrypt.compare(pwd, person.password, function (err, result) {
-                if(result === true) {
-                    res.render("secrets");
-                } else {
-                    console.log("Incorrect password");
-                }
-            })
+        req.login(user, function(err) {
+        if (err) {
+            console.log(err);
         } else {
-            console.log("No user found.")
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
         }
-        
-
-    } catch (err) {
-        console.log(err);
-
-    }
+    });   
+    
 });
+
 
 
 app.listen(3000, function() {
